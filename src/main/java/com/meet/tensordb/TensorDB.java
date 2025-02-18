61cacb805ac68f5e3651a68b4f865df8e6326edc
Meet Dawda <94282163+dawdameet@users.noreply.github.com>
2025-02-18 20:49:40 +0530

    minor adjustments

--- src/main/java/com/meet/tensordb/TensorDB.java
+++ src/main/java/com/meet/tensordb/TensorDB.java
@@ -1,23 +1,15 @@
 package com.meet.tensordb;
-
 public class TensorDB {
     static {
         System.load(System.getProperty("user.dir") + "/tensorlib.dll");
     }
-
-    // Native methods (linked to Rust)
     public native void store(String key, String data);
     public native String get(String key);
-
     public static void main(String[] args) {
         TensorDB db = new TensorDB();
-
-        // Store a tensor
         String tensorJson = "[[1.0, 2.0], [3.0, 4.0]]";
         db.store("tensor1", tensorJson);
         System.out.println("Stored tensor: " + tensorJson);
-
-        // Retrieve the tensor
         String retrievedTensor = db.get("tensor1");
         System.out.println("Retrieved tensor: " + retrievedTensor);
     }
